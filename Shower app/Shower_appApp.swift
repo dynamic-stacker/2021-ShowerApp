f85488e0ce899c8e19ea7ee778a59db93282a925
James Chan <james_chan@rhvs.edu.sg>
2021-11-11 20:47:13 +0800

    Initial Commit

---
+++ Shower app/Shower_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shower_appApp.swift
+//  Shower app
+//
+//  Created by Student on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Shower_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
